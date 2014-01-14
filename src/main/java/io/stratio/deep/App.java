735c203a276a5ba613e88d456b0e26d9f07bc391
Miguel Angel Fernandez Diaz <mafernandez@paradigmatecnologico.com>
2014-01-14 12:45:46 +0100

    initial project version

---
+++ src/main/java/io/stratio/deep/App.java
@@ -0,0 +1,13 @@
+package io.stratio.deep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
